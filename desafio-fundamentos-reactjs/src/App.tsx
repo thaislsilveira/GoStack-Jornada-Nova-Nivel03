21fd3a2539de7eb742f59dca0bdca74fb20b261a
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-04 09:16:26 -0300

    Página de login

--- desafio-fundamentos-reactjs/src/App.tsx
+++ desafio-fundamentos-reactjs/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
     <GlobalStyle />
+    <Router>
+      <Routes />
+    </Router>
   </>
 );
 
