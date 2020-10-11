62cc0d0fa8cb8536ab7008efd1115ad78a4e3d9d
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-11 18:25:24 -0300

    Configurando Rotas

--- 06-gobarber-web/src/pages/SignIn/styles.ts
+++ 06-gobarber-web/src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgoundImg from '../../assets/sign-in-background.png';
@@ -12,12 +12,33 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   display: flex;
-  place-content: center;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+from {
+  opacity: 0;
+  transform: translatex(-50px);
+} to{
+
+  opacity: 1;
+  transform: translatex(0);
+
+}
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -60,7 +81,6 @@ export const Content = styled.div`
     }
   }
 `;
-
 export const Background = styled.div`
   flex: 1;
   background: url(${signInBackgoundImg}) no-repeat center;
--- 06-gobarber-web/src/pages/SignUp/index.tsx
+++ 06-gobarber-web/src/pages/SignUp/index.tsx
@@ -1,7 +1,11 @@
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import {
+ FiArrowLeft, FiMail, FiLock, FiUser
+} from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
 import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -10,7 +14,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface FormData {
   name: string;
@@ -46,26 +50,28 @@ const SignUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1> Faça seu cadastro</h1>
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1> Faça seu cadastro</h1>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
 
-          <Button type="submit">Cadastrar</Button>
-        </Form>
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <a href="login">
-          <FiArrowLeft />
-          Voltar pra logon
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar pra logon
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
