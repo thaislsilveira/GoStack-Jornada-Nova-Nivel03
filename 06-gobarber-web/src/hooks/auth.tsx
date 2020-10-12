5390ce03ee8a960dc7f2faad1218cf8e9e26bc16
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-11 21:19:49 -0300

    rotas autenticadas

--- 06-gobarber-web/src/hooks/auth.tsx
+++ 06-gobarber-web/src/hooks/auth.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useCallback, useState, useContext } from 'react';
+import React, {
+ createContext, useCallback, useState, useContext
+} from 'react';
 import api from '../services/api';
 
 interface AuthState {
