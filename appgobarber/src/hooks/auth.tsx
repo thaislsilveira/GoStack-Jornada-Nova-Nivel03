22732ca114b721284357d1b23a58ed8eb86cb271
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-20 14:41:39 -0300

    Rotas privadas

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -24,6 +24,7 @@ interface AuthContextData {
     email: string;
     avatar: string;
   };
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -32,6 +33,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -40,6 +42,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -68,7 +71,10 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{
+      user: data.user, loading, signIn, signOut,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );
