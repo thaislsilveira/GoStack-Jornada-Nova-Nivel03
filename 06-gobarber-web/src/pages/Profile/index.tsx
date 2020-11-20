235e8f413b6435f6733cb62c5ce8c8791d5b4b41
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-20 15:37:57 -0300

    troca de avatar

--- 06-gobarber-web/src/pages/Profile/index.tsx
+++ 06-gobarber-web/src/pages/Profile/index.tsx
@@ -30,7 +30,7 @@ const Profile: React.FC = () => {
   const { addToast } = useToast();
   const history = useHistory();
 
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: ProfileFormData) => {
@@ -118,6 +118,8 @@ const Profile: React.FC = () => {
         data.append('avatar', e.target.files[0]);
 
         api.patch('/users/avatar', data).then(response => {
+          updateUser(response.data);
+
           addToast({
             type: 'success',
             title: 'Avatar atualizado!',
@@ -125,7 +127,7 @@ const Profile: React.FC = () => {
         });
       }
     },
-    [addToast],
+    [addToast, updateUser],
   );
 
   return (
