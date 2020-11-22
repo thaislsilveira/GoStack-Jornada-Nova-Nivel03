e0c5c9473cfa2b42b74c4640f169720cd34964f7
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-22 17:01:15 -0300

    começando a criar páginas

---
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AppoitmentCreated: React.FC = () => {
+  return <View />;
+};
+// parei em 04:10
+export default AppoitmentCreated;
