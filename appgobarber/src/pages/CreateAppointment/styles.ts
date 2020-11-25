0275efeb092786ad7ba761569bbaf1fbfab7a370
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-25 10:28:14 -0300

    Estrutura da criação

--- appgobarber/src/pages/CreateAppointment/styles.ts
+++ appgobarber/src/pages/CreateAppointment/styles.ts
@@ -1,3 +1,30 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  background: #28262e;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+  line-height: 28px;
+  margin-left: 16px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
