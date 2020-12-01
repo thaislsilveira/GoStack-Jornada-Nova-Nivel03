f42e7c092d0b23a968f99c470d4355a2d8bec35a
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-12-01 14:27:19 -0300

    Criando tela de perfil

--- appgobarber/src/pages/Dashboard/index.tsx
+++ appgobarber/src/pages/Dashboard/index.tsx
@@ -33,7 +33,7 @@ interface Provider {
 const Dashboard: React.FC = () => {
   const [providers, setProviders] = useState<Provider[]>([]);
 
-  const { signOut, user } = useAuth();
+  const { user } = useAuth();
   const { navigate } = useNavigation();
 
   useEffect(() => {
@@ -43,8 +43,8 @@ const Dashboard: React.FC = () => {
   }, []);
 
   const navigateToProfile = useCallback(() => {
-    signOut();
-  }, [signOut]);
+    navigate('Profile');
+  }, [navigate]);
 
   const navigateToCreateAppoitnment = useCallback(
     (providerId: string) => {
